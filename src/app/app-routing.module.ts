ddd5bf7681486cfa694e0f7f86e0436682080229
Andre Ishikawa <andremishikawa@yahoo.com.br>
2021-02-17 20:44:11 -0300

    Projeto router angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { LocalComponent } from './local/local.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'cadastro', component: CadastroComponent
+},
+{ 
+  path: 'local', component: LocalComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
